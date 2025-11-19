161265033c829806b113e6b42515ee3251c438a3
Top-Thanwa <phanetramtop@gmail.com>
2025-11-19 09:28:23 +0700

    Add project files.

---
+++ ConsoleHelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleHelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("My name is Thanwa Phanetram");
+            Console.WriteLine("Computer science Program");
+            Console.WriteLine("------------+++------------");
+            Console.WriteLine("My Hometown is");
+            Console.WriteLine("Nongkhai");
+            Console.WriteLine("Thailand");
+        }
+    }
+}

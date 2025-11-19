6cd9b7c94853fc0d0cdda343d1aeffef8fd3a43a
Top-Thanwa <phanetramtop@gmail.com>
2025-11-19 10:44:23 +0700

    d

--- ConsoleHelloWorld/Program.cs
+++ ConsoleHelloWorld/Program.cs
@@ -10,7 +10,7 @@
             Console.WriteLine();
             Console.Write("Please input your name (ENG) : ");
             name = Console.ReadLine();
-             
+            //gggggggggggggggg
 
             Console.WriteLine("Hello, Welcome "+name);
 
